5a51639fac4e77bfb48f6f31a1ea4401e6291907
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-08-09 00:43:42 +0200

    vercel test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import path from 'path';
 
 export function getStaticProps() {
+    // global.fonts.OXANIUM_8_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_8_white.fnt`)
+    // global.fonts.OXANIUM_12_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_12_white.fnt`)
+    // global.fonts.OXANIUM_14_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_14_white.fnt`)
+    // global.fonts.OXANIUM_20_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_20_white.fnt`)
+    // global.fonts.OXANIUM_24_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_24_white.fnt`)
+
     path.resolve(process.cwd(), 'fonts');
-    path.resolve(process.cwd(), 'images', 'battle');
+    path.resolve(process.cwd(), 'images/battle');
+    console.log('test')
     return {
         props: {
             a: 0
