7683d4d847e910161f94bc642d69a795fe455026
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-08-08 23:59:28 +0200

    vercel bugfix

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import path from 'path';
 export function getStaticProps() {
     path.resolve(process.cwd(), 'fonts');
     path.resolve(process.cwd(), 'images', 'battle');
+    return {
+        props: {
+            a: 0
+        }
+    }
 }
 
 export default function Home() {
