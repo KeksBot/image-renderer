69f97a3d9058b4de2c41d82ac481f3e83d1988dc
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-08-09 20:32:23 +0200

    more tests

--- fonts/register.js
+++ fonts/register.js
@@ -1,4 +1,13 @@
 const Jimp = require('jimp')
+import image1 from './3jhZXPpA0WfjMiDUoTJHs0zJ.ttf_0.png'
+import image2 from './d23wc7c5ypY72NuGMhRPed6a.ttf_0.png'
+import image3 from './gHP3L_gjp_hp3pQorAvGzGa7.ttf_0.png'
+import image4 from './kXWsn3L1zGM8NvLOn19v1A6g.ttf_0.png'
+import font8 from './OxaniumLight_8_white.fnt'
+import font12 from './OxaniumLight_12_white.fnt'
+import font14 from './OxaniumLight_14_white.fnt'
+import font20 from './OxaniumLight_20_white.fnt'
+import font24 from './OxaniumLight_24_white.fnt'
 
 module.exports = async function() {
     global.fonts = {}
