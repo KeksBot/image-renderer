fd28025cd75f2895d3dc9dde8c8bf86a072854b5
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-08-09 20:27:15 +0200

    more tests

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 
-export function getStaticProps() {
+export function getServerSideProps() {
     // global.fonts.OXANIUM_8_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_8_white.fnt`)
     // global.fonts.OXANIUM_12_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_12_white.fnt`)
     // global.fonts.OXANIUM_14_WHITE = await Jimp.loadFont(`${fontFolder}OxaniumLight_14_white.fnt`)
@@ -11,7 +11,6 @@ export function getStaticProps() {
     // path.resolve(process.cwd(), 'fonts');
     // path.resolve(process.cwd(), 'images/battle');
     let contents = fs.readdirSync(path.resolve(process.cwd(), 'fonts'))
-    console.log('test')
     return {
         props: {
             a: contents
